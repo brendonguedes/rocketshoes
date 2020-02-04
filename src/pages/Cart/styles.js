da6cdd8a40f409de4f551d36aad97ca5416948ad
brendonguedes <lordbguedes@gmail.com>
2020-02-04 19:21:59 -0300

    estilização do carrinho, uso do polished para alterar as cores

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,9 +1,32 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
   padding: 30px;
   background: #fff;
   border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
+      }
+    }
+  }
 `;
 
 export const ProductTable = styled.table`
@@ -36,6 +59,37 @@ export const ProductTable = styled.table`
     font-size: 18px;
     font-weight: bold;
   }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
 `;
 
-export const Total = styled.div``;
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
