28b18c046164bad16753abe63c8f6a8dcaf8d8a0
brendonguedes <lordbguedes@gmail.com>
2020-03-01 01:19:58 -0300

    edit actions, rootSaga, saga.js and others

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,1 +1,11 @@
-function* addToCart(action) {}
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import api from '../../../services/api';
+import { addToCartSuccess } from './actions';
+
+function* addToCart({ id }) {
+  const response = yield call(api.get, `/products/${id}`);
+
+  yield put(addToCartSuccess(response.data));
+}
+
+export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
